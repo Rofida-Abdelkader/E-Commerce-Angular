361772e5fde05c979b1355663e80d3b55f6ac640
Alyaa-Khalaf <alyaakhalaf729@gmail.com>
2026-05-04 00:13:49 +0300

    feat: add cart-checkout routes

--- E-Commerce-App/src/app/features/cart-checkout/cart-checkout.routes.ts
+++ E-Commerce-App/src/app/features/cart-checkout/cart-checkout.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const CART_ROUTES: Routes = [];
+export const CART_ROUTES: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/cart/cart').then((m) => m.CartComponent),
+  },
+  {
+    path: 'checkout',
+    loadComponent: () =>
+      import('./components/checkout/checkout').then((m) => m.CheckoutComponent),
+  },
+];
