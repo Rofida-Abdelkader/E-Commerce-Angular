d2327e3238a15e9cb9c55ece94e3caf3e9fa8f13
Shimaa Ameen <shimaaameen976@gmail.com>
2026-05-05 10:22:41 +0300

    Improve the ui of products and sidebar

--- E-Commerce-App/src/app/features/storefront/components/cart-sidebar/cart-sidebar.ts
+++ E-Commerce-App/src/app/features/storefront/components/cart-sidebar/cart-sidebar.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartItem } from '../../model/cart.model';
 
@@ -10,9 +10,14 @@ import { CartItem } from '../../model/cart.model';
   styleUrl: './cart-sidebar.scss'
 })
 export class CartSidebarComponent {
+     
+  @Output() increase = new EventEmitter<number>();
+@Output() decrease = new EventEmitter<number>();
+@Output() close = new EventEmitter<void>();
 
   @Input() cart: CartItem[] = [];
   @Input() visible = false;
+  
 
   getTotal(): number {
     return this.cart.reduce(
--- E-Commerce-App/src/app/features/storefront/components/product-list/product-list.ts
+++ E-Commerce-App/src/app/features/storefront/components/product-list/product-list.ts
@@ -92,4 +92,31 @@ export class ProductList {
   getCartCount(): number {
     return this.cart.reduce((sum, item) => sum + item.quantity, 0);
   }
+
+  // quantity
+  increaseQty(productId: number) {
+  const item = this.cart.find(i => i.product.id === productId);
+  if (item) {
+    item.quantity++;
+    this.saveCart();
+  }
+}
+
+decreaseQty(productId: number) {
+  const index = this.cart.findIndex(i => i.product.id === productId);
+
+  if (index > -1) {
+    if (this.cart[index].quantity > 1) {
+      this.cart[index].quantity--;
+    } else {
+      this.cart.splice(index, 1); // 👈 remove لو بقت 0
+    }
+
+    this.saveCart();
+  }
+}
+
+saveCart() {
+  localStorage.setItem('cart', JSON.stringify(this.cart));
+}
 }
